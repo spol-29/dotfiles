365060abd844a61cf7ab92784178e13c38de85f2
spol29 <68199583+spol-29@users.noreply.github.com>
2024-07-29 19:06:09 -0500

    Delete nvim directory

--- nvim/after/plugin/jukit.lua
+++
@@ -1,9 +0,0 @@
-
--- Enable highlighting of cell markers
-vim.g.jukit_highlight_markers = 1
-
--- Set the highlight colors for cell markers and text cell background
-vim.cmd [[
-  highlight jukit_cellmarker_colors guifg=#4e4e4e guibg=#4e4e4e ctermbg=238 ctermfg=238
-  highlight jukit_textcell_bg_colors guibg=#3a3a3a ctermbg=237
-]]
